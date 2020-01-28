a063741e325ef2dfb57755f6946d9e9356aaa391
Marcelo Mariduena <marcelomariduena10@gmail.com>
2020-01-28 10:08:51 -0500

    formatting

--- public/main.js
+++ public/main.js
@@ -2,9 +2,7 @@ function toggleSwitch() {
     let topNav = document.getElementById("topNav");
     if (topNav.className === "topnav") {
       topNav.className += " responsive";
-      alert("responsive added");
     } else {
       topNav.className = "topnav";
-      alert("responsive removed");
     }
 }
